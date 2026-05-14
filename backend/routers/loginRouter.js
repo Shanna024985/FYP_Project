d5b496ad552c0ff0d17dca22d59989120b4d94e4
Phoenix226-a <cedricmok.24@ichat.sp.edu.sg>
2026-05-15 04:14:47 +0800

    [MINOR] added userModel and more loginController functions. redirect_uri is not decided yet, this will be changed soon

--- backend/routers/loginRouter.js
+++ backend/routers/loginRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const loginController = require('../controllers/loginController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
 const router = express.Router();
 
 router.post('/', loginController.createSingpassURL);
+router.post('/token', loginController.checkRedirectIsValid, loginController.getSingpassToken, loginController.checkSingpassIdExists, jwtMiddleware.generateToken, loginController.processJSON);
 
 module.exports = router;
