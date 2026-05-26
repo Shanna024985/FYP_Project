81120d3bc3f57611f7bb67a77453874318d145cc
Huang-Shiying <HSHIYING.24@ichat.sp.edu.sg>
2026-05-26 17:50:46 +0800

    Merge branch 'auth' into Login-Singpass

--- backend/routers/mainRoutes.js
+++ backend/routers/mainRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const jobRoutes = require("./jobRoutes");
 
 router.get("/", (req, res, next) => {
     res.send("You are connected!");
 });
 
+
+router.use("/jobs", jobRoutes);
+
 // routers
-const loginRouter = require("./loginRouter");
+const loginRouter = require('./loginRouter');
 router.use("/auth", loginRouter);
 
 module.exports = router;
