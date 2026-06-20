3e94234a9c69ae8c9fbb9817f5670ff1505bf6ae
Shanna <shannalee1307@outlook.com>
2026-06-20 16:45:46 +0800

    [MINOR] made the edit job panel for employer full stack

--- FYP_Project/src/DatePicker.tsx
+++ FYP_Project/src/DatePicker.tsx
@@ -1,95 +1,103 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { parseDate } from "chrono-node"
-import { CalendarIcon } from "lucide-react"
+import * as React from "react";
+import { parseDate } from "chrono-node";
+import { CalendarIcon } from "lucide-react";
 
-import { Calendar } from "@/components/ui/calendar"
-import { Field, FieldLabel } from "@/components/ui/field"
+import { Calendar } from "@/components/ui/calendar";
+import { Field, FieldLabel } from "@/components/ui/field";
 import {
   InputGroup,
   InputGroupAddon,
   InputGroupButton,
   InputGroupInput,
-} from "@/components/ui/input-group"
+} from "@/components/ui/input-group";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
 function formatDate(date: Date | undefined) {
   if (!date) {
-    return ""
+    return "";
   }
 
   return date.toLocaleDateString("en-US", {
     day: "2-digit",
     month: "long",
     year: "numeric",
-  })
+  });
 }
+type dateGiven = {
+  date?: Date;
+};
 
-export function DatePickerNaturalLanguage() {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("Select date")
+export function DatePickerNaturalLanguage(props: dateGiven) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("Select date");
   const [date, setDate] = React.useState<Date | undefined>(
-    parseDate(value) || undefined
-  )
+    parseDate(value) || undefined,
+  );
+  React.useEffect(() => {
+    if (props.date) {
+      setDate(props.date);
+      setValue(formatDate(props.date));
+    }
+  }, [props.date]);
 
   return (
-      <InputGroup>
-        <InputGroupInput
-          id="date-optional"
-          value={value}
-          placeholder="Tomorrow or next week"
-          onChange={(e) => {
-            setValue(e.target.value)
-            const date = parseDate(e.target.value)
-            if (date) {
-              setDate(date)
-            }
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "ArrowDown") {
-              e.preventDefault()
-              setOpen(true)
-            }
-          }}
-        />
-        <InputGroupAddon align="inline-end">
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <InputGroupButton
-                id="date-picker"
-                variant="ghost"
-                size="icon-xs"
-                aria-label="Select date"
-              >
-                <CalendarIcon />
-                <span className="sr-only">Select date</span>
-              </InputGroupButton>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto overflow-hidden p-0"
-              align="end"
-              sideOffset={8}
+    <InputGroup>
+      <InputGroupInput
+        id="date-optional"
+        value={value}
+        placeholder="Tomorrow or next week"
+        onChange={(e) => {
+          setValue(e.target.value);
+          const date = parseDate(e.target.value);
+          if (date) {
+            setDate(date);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "ArrowDown") {
+            e.preventDefault();
+            setOpen(true);
+          }
+        }}
+      />
+      <InputGroupAddon align="inline-end">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <InputGroupButton
+              id="date-picker"
+              variant="ghost"
+              size="icon-xs"
+              aria-label="Select date"
             >
-              <Calendar
-                mode="single"
-                selected={date}
-                captionLayout="dropdown"
-                defaultMonth={date}
-                onSelect={(date) => {
-                  setDate(date)
-                  setValue(formatDate(date))
-                  setOpen(false)
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        </InputGroupAddon>
-      </InputGroup>
-
-  )
+              <CalendarIcon />
+              <span className="sr-only">Select date</span>
+            </InputGroupButton>
+          </PopoverTrigger>
+          <PopoverContent
+            className="w-auto overflow-hidden p-0"
+            align="end"
+            sideOffset={8}
+          >
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              defaultMonth={date}
+              onSelect={(date) => {
+                setDate(date);
+                setValue(formatDate(date));
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </InputGroupAddon>
+    </InputGroup>
+  );
 }
--- FYP_Project/src/EditJobs.tsx
+++ FYP_Project/src/EditJobs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationMenus from "./NavigationMenu";
 import { Field, FieldGroup, FieldLabel } from "./components/ui/field";
 import { Input } from "./components/ui/input";
@@ -12,7 +12,149 @@ import { Button } from "./components/ui/button";
 type Props = {
   currentUrl: String;
 };
+type JobsObject = {
+  id: number;
+  company_id: number;
+  title: string;
+  description: string;
+  category: string;
+  type: string;
+  status: string;
+  salary_range_from: number;
+  salary_range_to: number;
+  salary_type: string;
+  salary_period: string;
+  duration: string;
+  deadline: string;
+  experience: string;
+  career_level: string;
+  location: string;
+  jobs_needed: number;
+  reports: string;
+  created_at: string;
+  updated_at: string;
+  company_name: string;
+  company_city: string;
+};
+type Statuses = {
+  status?: string;
+};
+let StatusOfJob = (props: Statuses) => {
+  let [options, setOptions] = useState(["Active", "Closed"]);
+  return (
+    <>
+      {options.map((value) => {
+        if (value == props.status) {
+          return (
+            <NativeSelectOption selected value={value}>
+              {value}
+            </NativeSelectOption>
+          );
+        } else {
+          return <NativeSelectOption value={value}>{value}</NativeSelectOption>;
+        }
+      })}
+    </>
+  );
+};
+let JobType = (props: Statuses) => {
+  let [options, setOptions] = useState([
+    "Full-time",
+    "Part-Time",
+    "Contract",
+    "GIG",
+  ]);
+  return (
+    <>
+      {options.map((value) => {
+        return (
+          <NativeSelectOption value={value} selected={value === props.status}>
+            {value}
+          </NativeSelectOption>
+        );
+      })}
+    </>
+  );
+};
+let LocationOptions = (props: Statuses) => {
+  let [options, setOptions] = useState(["Central", "West","East","North"]);
+  return (
+    <>
+      {options.map((value) => {
+        if (value == props.status) {
+          return (
+            <NativeSelectOption selected value={value}>
+              {value}
+            </NativeSelectOption>
+          );
+        } else {
+          return <NativeSelectOption value={value}>{value}</NativeSelectOption>;
+        }
+      })}
+    </>
+  );
+};
+let dateReturned = (stringOfDate: string) => {
+  if (stringOfDate) {
+    let date = new Date(stringOfDate);
+    console.log(date.toLocaleString());
+    return date;
+  }
+};
+let CareerLevel = (props: Statuses) => {
+  let [options, setOptions] = useState([
+    {
+      value: "entry",
+      shownOnPage: "Entry Level/Early Career",
+    },
+    {
+      value: "experienced",
+      shownOnPage: "Experienced Professional",
+    },
+    {
+      value: "leadership",
+      shownOnPage: "Leadership/Management",
+    },
+    {
+      value: "owner",
+      shownOnPage: "Independent/Owner",
+    },
+  ]);
+  return (
+    <>
+      {options.map((values) => {
+        if (props.status) {
+          if (values.value == props.status) {
+            return (
+              <NativeSelectOption selected value={values.value}>
+                {values.shownOnPage}
+              </NativeSelectOption>
+            );
+          } else {
+            return (
+              <NativeSelectOption value={values.value}>{values.shownOnPage}</NativeSelectOption>
+            );
+          }
+        }
+      })}
+    </>
+  );
+};
 const EditJobs = (props: Props) => {
+  let [jobReturned, setJobReturned] = useState<JobsObject>();
+  useEffect(() => {
+    let address = new URL(window.location.href);
+    let queryParameters = address.searchParams;
+    let id = queryParameters.get("id");
+    fetch(props.currentUrl + "/jobs/" + id)
+      .then((value) => {
+        return value.json();
+      })
+      .then((result) => {
+        let job = result.job;
+        setJobReturned(job);
+      });
+  }, []);
   return (
     <div>
       <NavigationMenus />
@@ -21,76 +163,81 @@ const EditJobs = (props: Props) => {
         <FieldGroup className="mt-8">
           <Field>
             <FieldLabel htmlFor="titleName">Job Title</FieldLabel>
-            <Input id="titleName" placeholder="Backend Engineer" />
+            <Input
+              id="titleName"
+              placeholder="Backend Engineer"
+              value={jobReturned?.title}
+            />
           </Field>
           <Field>
             <FieldLabel htmlFor="description">Description</FieldLabel>
-            <Textarea id="description" placeholder="We need..." />
+            <Textarea
+              id="description"
+              placeholder="We need..."
+              value={jobReturned?.description}
+            />
           </Field>
           <Field>
             <FieldLabel htmlFor="jobTypes">Job Type</FieldLabel>
             <NativeSelect id="jobTypes">
-              <NativeSelectOption value={"fullTime"}>
-                Full-time
-              </NativeSelectOption>
-              <NativeSelectOption value={"partTime"}>
-                Part-time
-              </NativeSelectOption>
-              <NativeSelectOption value={"contract"}>
-                Contract
-              </NativeSelectOption>
-              <NativeSelectOption value={"GIG"}>GIG</NativeSelectOption>
+              <JobType status={jobReturned?.type} />
             </NativeSelect>
           </Field>
           <Field>
             <FieldLabel htmlFor="statusDropdown">Status</FieldLabel>
             <NativeSelect id="statusDropdown">
-              <NativeSelectOption value={"Active"}>Active</NativeSelectOption>
-              <NativeSelectOption value={"Closed"}>Closed</NativeSelectOption>
+              <StatusOfJob status={jobReturned?.status} />
             </NativeSelect>
           </Field>
           <Field>
             <FieldLabel htmlFor="locationDropdown">Location</FieldLabel>
             <NativeSelect id="locationDropdown">
-              <NativeSelectOption value={"central"}>Central</NativeSelectOption>
-              <NativeSelectOption value={"west"}>West</NativeSelectOption>
-              <NativeSelectOption value={"east"}>East</NativeSelectOption>
-              <NativeSelectOption value={"North"}>North</NativeSelectOption>
+              {/* <NativeSelectOption value={"Central"}>Central</NativeSelectOption>
+              <NativeSelectOption value={"West"}>West</NativeSelectOption>
+              <NativeSelectOption value={"East"}>East</NativeSelectOption>
+              <NativeSelectOption value={"North"}>North</NativeSelectOption> */}
+              <LocationOptions status={jobReturned?.location}/>
             </NativeSelect>
           </Field>
           <Field>
             <FieldLabel htmlFor="date-optional">
               Application Deadline
             </FieldLabel>
-            <DatePickerNaturalLanguage />
+            <DatePickerNaturalLanguage
+              date={dateReturned(jobReturned?.deadline ?? "")}
+            />
           </Field>
           <Field>
             <FieldLabel htmlFor="noofjobs">Number of jobs needed</FieldLabel>
-            <Input id="noofjobs" type="number" placeholder="4" />
+            <Input
+              id="noofjobs"
+              type="number"
+              placeholder="4"
+              value={jobReturned?.jobs_needed}
+            />
           </Field>
           <Field>
             <FieldLabel htmlFor="minimumexperience">Career level</FieldLabel>
             <NativeSelect id="minimumexperience">
-              <NativeSelectOption value={"early"}>
-                Entry Level/Early Career
-              </NativeSelectOption>
-              <NativeSelectOption value={"experienced"}>
-                Experienced Professional
-              </NativeSelectOption>
-              <NativeSelectOption value={"leadership"}>
-                Leadership/Management
-              </NativeSelectOption>
-              <NativeSelectOption value={"independent"}>
-                Independent/Owner
-              </NativeSelectOption>
+              <CareerLevel status={jobReturned?.career_level}/>
             </NativeSelect>
           </Field>
           <Field>
             <FieldLabel>Salary Range</FieldLabel>
             <div className="flex justify-between">
-              <Input id="minsalary" className="w-[45%]" placeholder="100" />
+              <Input
+                id="minsalary"
+                className="w-[45%]"
+                placeholder="100"
+                value={jobReturned?.salary_range_from}
+              />
               <p className="text-center p-1">to</p>
-              <Input id="maxsalary" className="w-[45%]" placeholder="300" />
+              <Input
+                id="maxsalary"
+                className="w-[45%]"
+                placeholder="300"
+                value={jobReturned?.salary_range_to}
+              />
             </div>
           </Field>
         </FieldGroup>
