7cb86210a6ca1bfcace8663c4d52d9629cadf677
Shanna <shannalee1307@outlook.com>
2026-07-08 17:36:17 +0800

    [MINOR] changed to allow api to work

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,12 @@ app.use(express.urlencoded({extended: false}))
 // Singpass also needs session data
 app.use(session({secret: 'singpassSessionData', resave: false, saveUninitialized: true, cookie: {secure: false, maxAge: 120000}}))
 // socket for updating messages
-let pathForServingHtmlFile = path.join(__dirname,"dist")
-console.log(pathForServingHtmlFile)
-app.use("/",express.static(pathForServingHtmlFile))
+
 
 let mainRoutes = require("./routers/mainRoutes")
 app.use("/api",mainRoutes)
 
-// Catch-all for React Router
-app.use((req, res) => {
-    res.sendFile(path.join(pathForServingHtmlFile, "index.html"));
-});
+
 
 // START THE SERVER - ADD THIS
 const PORT = process.env.PORT || 3000;
