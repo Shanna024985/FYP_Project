92800d84939b26886cf983252e490d3a31ce205e
Phoenix226-a <cedricmok.24@ichat.sp.edu.sg>
2026-07-09 23:30:30 +0800

    [BUGFIX] fixed jobId parameter for updating status of application

--- backend/routers/jobRoutes.js
+++ backend/routers/jobRoutes.js
@@ -11,7 +11,7 @@ router.get("/my/companies", jwtMiddleware.verifyToken, jobController.getMyCompan
 router.get("/my/jobs", jwtMiddleware.verifyToken, jobController.getMyJobs);
 
 // update status of application
-router.put("/application/:id", jwtMiddleware.verifyToken, applicationController.verifyStatus, applicationController.getJobIdByApplicationId, applicationController.verifyJobOwnership, applicationController.updateStatusById);
+router.put("/application/:jobId", jwtMiddleware.verifyToken, applicationController.verifyStatus, applicationController.getJobIdByApplicationId, applicationController.verifyJobOwnership, applicationController.updateStatusById);
 
 // Public job routes
 router.get("/", jobController.getAllJobs);
