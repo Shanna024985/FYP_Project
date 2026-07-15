28973c997f2c4d2fdb4b4682479465731d9b073d
Shanna <shannalee1307@outlook.com>
2026-07-15 16:47:14 +0800

    [MINOR] made small changes to allow profile images to show up

--- FYP_Project/src/Messages.tsx
+++ FYP_Project/src/Messages.tsx
@@ -3,6 +3,12 @@ import React from "react";
 import { Input } from "./components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
 import { Button } from "./components/ui/button";
+import { Bubble, BubbleContent } from "./components/ui/bubble";
+import {
+  Message,
+  MessageAvatar,
+  MessageContent,
+} from "./components/ui/message";
 type Props = {
   currentUrl: String;
 };
@@ -310,16 +316,50 @@ const Messages = (props: Props) => {
             <Button className="bg-blue-500 self-center">View profile</Button>
           </div>
           <div className="bg-gray-200 mt-4 h-[82vh] flex flex-col justify-end gap-4">
-            <p className="text-gray-500">
+            <div className="p-4 flex flex-col gap-5">
+              {/* <p className="text-gray-500">
               All of your chat history will be shown here
-            </p>
+            </p> */}
+              <Message>
+                <MessageAvatar>
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </MessageAvatar>
+                <MessageContent>
+                  <Bubble variant={"muted"}>
+                    <BubbleContent>How can I help you today?</BubbleContent>
+                  </Bubble>
+                </MessageContent>
+              </Message>
+              <Message align="end">
+                <MessageAvatar>
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </MessageAvatar>
+                <MessageContent>
+                  <Bubble>
+                    <BubbleContent>How can I help you today?</BubbleContent>
+                  </Bubble>
+                </MessageContent>
+              </Message>
+            </div>
             <div className="flex mb-3 gap-3 p-2">
               <PlusCircle className="self-center" />
               <Input
                 placeholder=""
                 className="bg-white  border-black self-center"
               />
-              <SmileIcon className="self-center"/>
+              <SmileIcon className="self-center" />
               <Button>Send</Button>
             </div>
           </div>
