a198769ae349077e96818eeeb3b5a4a1fcaaf15d
Phoenix226-a <cedricmok.24@ichat.sp.edu.sg>
2026-07-22 17:38:30 +0800

    [BUGFIX] fixed messageModel not connecting to render websocket

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -126,7 +126,7 @@ module.exports.sendUpdateMessage = (userId, senderUserId) => {
     jwt.sign({"secret_key":  websocketSecretKey}, jwtSecretKey, options, (err, encoded) => {
         jose.importJWK(JSON.parse(websocketPublicKey), 'ECDH-ES+A256KW').then(publicKeyCrypto => {
             new jose.CompactEncrypt(new TextEncoder().encode(encoded)).setProtectedHeader(JSON.parse(websocketAlg)).encrypt(publicKeyCrypto).then(token => {
-                const socket = new WebSocket(`wss://fyp-project-fkmo.onrender.com?admin_token=${token}`);
+                const socket = new WebSocket(`wss://fyp-project-fkmo.onrender.com/?admin_token=${token}`);
 
                 socket.addEventListener('open', ws => {
                     console.log('Connected!');
